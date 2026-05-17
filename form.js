c2cee02c295a220f41649ed19c786f46ee0cd59a
mbl525 <mbl525@york.ac.uk>
2026-05-17 23:10:39 +0100

    v1.6

--- form.js
+++ form.js
@@ -1,22 +1,26 @@
-// https://github.com/IM-WADD/Lecture-Examples/blob/main/Lectures/W8Lecture1/storeClocksComplete/main.js
+/* 
+This code is based upon WADD W8L1 example on Storing and Displaying Data Locally
+Author: ace01603
+Location: https://github.com/IM-WADD/Lecture-Examples/blob/main/Lectures/W8Lecture1/storeClocksComplete/main.js
+Accessed: 12.05.2026
 
-// let concerts = [] // array to store concert objects
+This code also includes a function based upon a combinatino of MangoHost and Gemini
+Author: MangoHost and Gemini
+Location: https://github.com/IM-WADD/Lecture-Examples/blob/main/Lectures/W8Lecture1/storeClocksComplete/main.js
+Accessed: 12.05.2026
+*/
 
 let concerts = JSON.parse(localStorage.getItem("concerts")) || [];
 
 // handles form submission & creates a new concert object
 async function createConcert(event) {
     event.preventDefault();
-
     const imageFiles = document.getElementById("polImg") .files;
-
     const savedImages = [];
-
     for (let i = 0; i < imageFiles.length && i < 3; i++) {
         const imageData = await readImage(imageFiles[i]);
         savedImages.push(imageData);
     }
-
     const newConcert = {
         who: document.getElementById("who").value,
         when: document.getElementById("when").value,
@@ -26,19 +30,13 @@ async function createConcert(event) {
         notes: document.getElementById("notes").value,
         polImg: savedImages
     };
-    
-    // addNewConcert(newConcert);
-
     concerts.push(newConcert);
-
     saveConcerts();
-
     window.location.href = "journal.html"
-
     document.getElementById("concert-form").reset;
 }
 
-// credit: stack overflow! https://stackoverflow.com/questions/63198902/resolve-promises-one-after-another-for-previewing-image-on-upload
+// The following function is based on a combination of MangoHost's Tutorial and Gemini
 function readImage(file) {
     return new Promise(function(resolve) {
         const reader = new FileReader();
@@ -48,11 +46,11 @@ function readImage(file) {
         reader.readAsDataURL(file);
     });
 }
+// End of Mango Host and Gemini usage
 
-// saves concerts array to local storage
+// saves concerts as an array to local storage
 function saveConcerts() {
     localStorage.setItem("concerts", JSON.stringify(concerts));
 }
 
-// add event listener
 document.getElementById("submit").addEventListener("click", e => createConcert(e));
--- journal.js
+++ journal.js
@@ -1,8 +1,11 @@
-// https://github.com/IM-WADD/Lecture-Examples/blob/main/Lectures/W8Lecture1/storeClocksComplete/main.js
-// split the practical into two files, one for form + one for journal/showing data
+/* 
+This code is also based upon WADD W8L1 example on Storing and Displaying Data Locally, focusing on displaying data
+Author: ace01603
+Location: https://github.com/IM-WADD/Lecture-Examples/blob/main/Lectures/W8Lecture1/storeClocksComplete/main.js
+Accessed: 12.05.2026
+*/
 
 // adds a concert entry to the page
-
 const leftPage = document.querySelector(".left-page");
 const rightPage = document.querySelector(".right-page");
 
@@ -10,50 +13,35 @@ let currentConcert = 0;
 
 const concerts = JSON.parse(localStorage.getItem("concerts")) || [];
 
-document.getElementById("next-page")
-    .addEventListener("click", nextConcert);
-
-document.getElementById("prev-page")
-    .addEventListener("click", previousConcert);
+document.getElementById("next-page").addEventListener("click", nextConcert);
+document.getElementById("prev-page").addEventListener("click", previousConcert);
 
 function displayConcert() {
-
     const concert = concerts[currentConcert];
-
     if(concerts.length === 0) {
         leftPage.innerHTML = "<h2>no concerts yet</h2>"
         rightPage.innerHTML = "";
         return;
     }
-
     leftPage.innerHTML =
-    `
-    <h1> ${concert.tourName}</h1>
-    <p>${concert.notes}</p>
-    `;
+    `<h1> ${concert.tourName}</h1> <p>${concert.notes}</p>`;
     rightPage.innerHTML =
-    `
-    <div class="info-card">
+    `<div class="info-card">
         <p><b>Who:</b> ${concert.who}</p>
         <p><b>When:</b> ${concert.when}</p>
         <p><b>Where:</b> ${concert.where}</p>
         <p><b>Rating:</b> ${"★".repeat(Number(concert.rating))}</p>
     </div>
     <div class="photo-strip">
-
     ${concert.polImg.map((image, index) =>
-
         `<div class="polaroid photo-${index + 1}">
-
-            <img src="${image}" class="concert-photo">
-
+            <img src="${image}" class="concert-photo" alt="Your Concert Photos">
         </div>`
-
     ).join("")}
-    </div>
-    `;
+    </div>`;
 }
 
+// 
 function nextConcert() {
     currentConcert++;
     if(currentConcert >= concerts.length) {
@@ -61,7 +49,7 @@ function nextConcert() {
     }
     displayConcert();
 }
-
+// 
 function previousConcert() {
     currentConcert--;
     if(currentConcert < 0) {
